5d961199003ba129c4fe1f22413306f9cc68dbef
Michael <lordrevan1999@web.de>
2024-03-10 22:33:41 +0100

    added ingress secret creation script

---
+++ k8s/create_ingress_secrets.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+openssl req -x509 -new -nodes -key ca.key -sha256 -subj "/CN=thinkcentre-m92p.fritz.box" -days 3650 -out ca.crt -extensions san -config <(
+echo '[req]';
+echo 'distinguished_name=req';
+echo '[san]';
+echo 'subjectAltName=DNS:thinkcentre-m929.fritz.box')
+
+
+microk8s kubectl create secret generic ingress-cert --from-file=ca.crt=ca.crt --from-file=ca.key=ca.key -n kube-system
+microk8s kubectl create secret generic ingress-cert --from-file=ca.crt=ca.crt --from-file=ca.key=ca.key -n baby-dimensions
+microk8s kubectl create secret generic ingress-cert --from-file=ca.crt=ca.crt --from-file=ca.key=ca.key -n price-tracker
+microk8s kubectl create secret generic ingress-cert --from-file=ca.crt=ca.crt --from-file=ca.key=ca.key -n blood-bowl-league-manager
+microk8s kubectl create secret generic ingress-cert --from-file=ca.crt=ca.crt --from-file=ca.key=ca.key -n radio-station-scraping
+microk8s kubectl create secret generic ingress-cert --from-file=ca.crt=ca.crt --from-file=ca.key=ca.key -n paypal-donation-mail-scraper
